60cfc2594e45374338261dcb90314375975ce7d4
jumadvu123 <47142026+jumadvu123@users.noreply.github.com>
2021-10-04 12:59:13 +0400

    Developers Tab

--- app/containers/DevelopersItem/index.js
+++ app/containers/DevelopersItem/index.js
@@ -35,7 +35,7 @@ const buttonStyle = {
 const aStyle = {
   color: '#539bf5',
   textDecoration: 'none',
-  fontSize: '20px',
+  fontSize: '16px',
   fontWeight: '600',
   '&:hover': {
     background: '#539bf5 !important',
@@ -54,12 +54,22 @@ const svgBottomStyle = {
 };
 const divStyle = {
   flexDirection: 'row',
-  width: '80%',
+  width: '5%',
+  textAlign: 'start',
+};
+const divSecStyle = {
+  flexDirection: 'row',
+  width: '35%',
+  textAlign: 'start',
+};
+const divThirdStyle = {
+  flexDirection: 'row',
+  width: '35%',
   textAlign: 'start',
 };
 const divSecondStyle = {
   flexDirection: 'row',
-  width: '20%',
+  width: '25%',
   textAlign: 'right',
 };
 const divToolStyle = {
@@ -75,7 +85,27 @@ const textStyle = {
   marginTop: '4px',
   marginBottom: '4px',
   fontFamily: 'Segoe UI',
-  fontSize: '14px',
+  fontSize: '12px',
+};
+const textHStyle = {
+  lineHeight: 1.25,
+  margin: 0,
+  color: 'rgb(83, 155, 245)',
+  marginTop: '4px',
+  marginBottom: '4px',
+  fontFamily: 'Segoe UI',
+  fontSize: '20px',
+  fontWeight: 600,
+};
+const textHSubStyle = {
+  width: 'auto',
+  lineHeight: 1.25,
+  margin: 0,
+  color: '#768390',
+  marginTop: '4px',
+  marginBottom: '4px',
+  fontFamily: 'Segoe UI',
+  fontSize: '16px',
 };
 const textSubStyle = {
   width: 'auto',
@@ -92,27 +122,54 @@ const textItemFirst = {
   marginLeft: 0,
 };
 const tooltipImage = {
-  width: '30%',
+  width: '25%',
 };
 const tooltipText = {
-  width: '65%',
-  marginLeft: '5%',
+  width: '75%',
 };
 const imageStyle = { width: 20, height: 20, borderRadius: 10, marginLeft: 5 };
 const toolTipimageStyle = {
-  width: '60px',
-  height: '60px',
-  borderRadius: '30px',
+  width: '50px',
+  height: '50px',
+  borderRadius: '25px',
   alignItems: 'center',
-  marginTop: '20px',
+  margin: '0px',
 };
 let DevelopersItem = ({ article }) =>
   article
     ? article.map((el, key) => (
         <article style={articleStyle} key={key}>
           <div style={divStyle}>
+            <p style={textStyle}>{el.rank}</p>
+          </div>
+          <div style={divSecStyle}>
+            <a style={{ ...aStyle, ...textSubStyle, ...divToolStyle }}
+                  href={el.url}
+                >
+                  <div style={tooltipImage}>
+                    <img
+                      src={el.avatar}
+                      style={toolTipimageStyle}
+                    />
+                  </div>
+                  <div style={tooltipText}>
+                    <h3 style={textHStyle}>
+                      {el.name}
+                    </h3>
+                    <p style={textHSubStyle}>{el.username}</p>
+                  </div>
+                </a>
+          </div>
+          <div style={divThirdStyle}>
+            <div style={{...divStyle,...{ width: '100%'}}}>
+                <p style={textStyle}>
+                <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" style={{...svgStyle,...{color: '#cc6b2c'}}}>
+                    <path fill-rule="evenodd" d="M7.998 14.5c2.832 0 5-1.98 5-4.5 0-1.463-.68-2.19-1.879-3.383l-.036-.037c-1.013-1.008-2.3-2.29-2.834-4.434-.322.256-.63.579-.864.953-.432.696-.621 1.58-.046 2.73.473.947.67 2.284-.278 3.232-.61.61-1.545.84-2.403.633a2.788 2.788 0 01-1.436-.874A3.21 3.21 0 003 10c0 2.53 2.164 4.5 4.998 4.5zM9.533.753C9.496.34 9.16.009 8.77.146 7.035.75 4.34 3.187 5.997 6.5c.344.689.285 1.218.003 1.5-.419.419-1.54.487-2.04-.832-.173-.454-.659-.762-1.035-.454C2.036 7.44 1.5 8.702 1.5 10c0 3.512 2.998 6 6.498 6s6.5-2.5 6.5-6c0-2.137-1.128-3.26-2.312-4.438-1.19-1.184-2.436-2.425-2.653-4.81z"></path>
+                </svg>
+                POPULAR REPO</p>
+            </div>
             <h1 style={textStyle}>
-              <A style={aStyle} href={el.url} target="_blank">
+              <A style={aStyle} href={el.popularRepository.url} target="_blank">
                 <svg
                   aria-hidden="true"
                   height="16"
@@ -127,147 +184,21 @@ let DevelopersItem = ({ article }) =>
                     d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
                   />
                 </svg>
-                <span style={spanStyle}>{el.username} / </span>
-                {el.repositoryName}
+                {el.popularRepository.repositoryName}
               </A>
             </h1>
-            <p style={textStyle}>{el.description}</p>
-            <div style={divStyle}>
-              <span style={{ ...textSubStyle, ...textItemFirst }}>
-                {el.language}
-              </span>
-              <A
-                style={{ ...aStyle, ...textSubStyle }}
-                href={el.url}
-                target="_blank"
-              >
-                <svg
-                  aria-label="star"
-                  role="img"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  version="1.1"
-                  width="16"
-                  data-view-component="true"
-                  style={svgBottomStyle}
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
-                  />
-                </svg>
-                {el.totalStars}
-              </A>
-              <A
-                style={{ ...aStyle, ...textSubStyle }}
-                href={el.url}
-                target="_blank"
-              >
-                <svg
-                  aria-label="fork"
-                  role="img"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  version="1.1"
-                  width="16"
-                  data-view-component="true"
-                  style={svgStyle}
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"
-                  />
-                </svg>
-                {el.forks}
-              </A>
-              <span style={textSubStyle}>Built by</span>
-              {el.builtBy.map((im, key1) => {
-                return (
-                  <a data-for={`so-close-${key}`} data-tip={key1}>
-                    <img src={im.avatar} style={imageStyle} />
-                  </a>
-                );
-              })}
-            </div>
+            <p style={textStyle}>{el.popularRepository.description}</p>
           </div>
-          <ReactTooltip
-            id={`so-close-${key}`}
-            getContent={dataTip => {
-              return (
-                <a
-                  style={{ ...aStyle, ...textSubStyle, ...divToolStyle }}
-                  href={
-                    el.builtBy[dataTip] ? el.builtBy[dataTip].url : '_blank'
-                  }
-                >
-                  <div style={tooltipImage}>
-                    <img
-                      src={
-                        el.builtBy[dataTip] ? el.builtBy[dataTip].avatar : ''
-                      }
-                      style={toolTipimageStyle}
-                    />
-                  </div>
-                  <div style={tooltipText}>
-                    <h3 style={textStyle}>
-                      {el.builtBy[dataTip] ? el.builtBy[dataTip].username : ''}
-                    </h3>
-                    <p style={textSubStyle}>API missing for details</p>
-                  </div>
-                </a>
-              );
-            }}
-            effect="solid"
-            delayHide={500}
-            delayShow={500}
-            delayUpdate={500}
-            place="top"
-            border={true}
-            backgroundColor="#2d333b"
-            color="#444c56"
-            borderColor="#444c56"
-          />
           <div style={divSecondStyle}>
             <button type="submit" style={buttonStyle}>
-              <svg
-                aria-hidden="true"
-                height="16"
-                viewBox="0 0 16 16"
-                version="1.1"
-                width="16"
-                data-view-component="true"
-                style={svgStyle}
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
-                />
-              </svg>{' '}
-              Star
+              Follow
             </button>
-            <p style={{ ...textStyle, ...{ marginTop: '25px' } }}>
-              <svg
-                aria-hidden="true"
-                height="16"
-                viewBox="0 0 16 16"
-                version="1.1"
-                width="16"
-                data-view-component="true"
-                style={svgStyle}
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
-                />
-              </svg>{' '}
-              {el.starsSince} stars {el.since === 'daily' ? 'today' : ''}
-            </p>
           </div>
         </article>
       ))
     : null;
 const mapStateToProps = state => ({
-  article: state.home ? state.home.repositories : [],
+  article: state.home ? state.home.developers : [],
 });
 DevelopersItem = connect(
   mapStateToProps,
